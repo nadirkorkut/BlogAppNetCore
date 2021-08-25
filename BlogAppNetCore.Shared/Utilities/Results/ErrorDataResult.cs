97f0aebea36097cfc40f1d39ea6439876e3968ed
nadirkorkut <korkutnadir@outlook.com>
2021-08-25 13:24:15 +0300

    Result Yapısı Oluşturuldu.

---
+++ BlogAppNetCore.Shared/Utilities/Results/ErrorDataResult.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppNetCore.Shared.Utilities.Results
+{
+    public class ErrorDataResult<T> : DataResult<T>
+    {
+        public ErrorDataResult(T data,string message):base(data,false,message)
+        {
+
+        }
+        public ErrorDataResult(T data):base(false,data)
+        {
+
+        }
+        public ErrorDataResult(string message):base(default, false, message)
+        {
+
+        }
+        public ErrorDataResult() : base(false, default)
+        {
+
+        }
+    }
+}

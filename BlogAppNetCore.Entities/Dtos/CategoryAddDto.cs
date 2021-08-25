0349806d876c3b7383fc7cba97a6b18ccb7f8ea3
nadirkorkut <korkutnadir@outlook.com>
2021-08-25 15:03:59 +0300

    Result Yapısı Güncellendi ve Category Servisi Yazıldı.

---
+++ BlogAppNetCore.Entities/Dtos/CategoryAddDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogAppNetCore.Entities.Dtos
+{
+    public class CategoryAddDto
+    {
+        [DisplayName("Kategori Adı")]
+        [Required(ErrorMessage = "{0} Boş Geçilmemelidir.")]
+        [MaxLength(70,ErrorMessage = "{0} {1} Karakterden Büyük Olmamalıdır.")]
+        [MinLength(3,ErrorMessage = "{0} {1} Karakterden Az Olmamalıdır.")]
+        public string Name { get; set; }
+        [DisplayName("Kategori Açıklaması")]
+        [MaxLength(500, ErrorMessage = "{0} {1} Karakterden Büyük Olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} Karakterden Az Olmamalıdır.")]
+        public string Description { get; set; }
+        [DisplayName("Kategori Özel Not Alanı")]
+        [MaxLength(500, ErrorMessage = "{0} {1} Karakterden Büyük Olmamalıdır.")]
+        [MinLength(3, ErrorMessage = "{0} {1} Karakterden Az Olmamalıdır.")]
+        public string Note { get; set; }
+        [DisplayName("Aktif Mi?")]
+        [Required(ErrorMessage = "{0} Boş Geçilmemelidir")]
+        public bool IsActive { get; set; }
+    }
+}
